cde3685123b6c4310a9f1b026f918e683606d84d
виталик рыбаков <vita250r@yandex.ru>
2018-03-17 19:16:50 +0300

    asd

--- test01.py
+++ test01.py
@@ -4,4 +4,10 @@ Created on Sat Mar 17 16:37:07 2018
 
 @author: 123
 """
+import telebot
+bot = telebot.TeleBot('481180883:AAFjGPMJVPKL0xaH2MksG4GlH5bprwwxdpI')
 
+
+@bot.message_handler(commands = ['start'])
+def start_bot(message):
+    bot.send_message(message.chat.id, "Привет")
